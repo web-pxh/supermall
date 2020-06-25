e1586bb32ef74208a67bc9f8e14b5bc83f9cba36
web-pxh <1448670742@qq.com>
2020-06-25 16:30:28 +0800

    刚开始

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,45 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Home"
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import { getHomeMultidata } from "network/home";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    };
+  },
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  created() {
+    //1、请求多个数据
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
   }
+};
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
